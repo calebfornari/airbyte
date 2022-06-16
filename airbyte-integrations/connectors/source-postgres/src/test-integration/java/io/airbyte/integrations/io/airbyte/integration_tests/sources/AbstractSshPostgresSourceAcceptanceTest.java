b497b31348d4528d66673501045b47ecc810ad19
jdpgrailsdev <jpearlin1@gmail.com>
2022-06-16 16:06:24 -0400

    Use per-stream for postgres tests

--- airbyte-integrations/connectors/source-postgres/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/AbstractSshPostgresSourceAcceptanceTest.java
+++ airbyte-integrations/connectors/source-postgres/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/AbstractSshPostgresSourceAcceptanceTest.java
@@ -129,4 +129,8 @@ public abstract class AbstractSshPostgresSourceAcceptanceTest extends SourceAcce
     return Jsons.jsonNode(new HashMap<>());
   }
 
+  @Override
+  protected boolean supportsPerStream() {
+    return true;
+  }
 }
